2a7fadf08f0086db33cb44ea5adb13019bfff961
Jocabed Ríos <j0c4b3d@gmail.com>
2023-01-03 09:11:16 -0600

    Returns a JSON object.

--- main.go
+++ main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"database/sql"
-	"encoding/json"
 	"fmt"
-	"os"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
 )
 
@@ -19,12 +19,14 @@ const (
 
 type User struct {
 	Id                int
+	Username          string
 	Name              string
 	Birth_date        string
 	Registration_date string
 }
 
-func main() {
+func getUserByUsername(c *gin.Context) {
+
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
@@ -38,34 +40,36 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
 	fmt.Println("Successfully connected!")
 
-	username := "jduesterrr"
-
-	rows, err := db.Query("SELECT id, name, birth_date, registration_date FROM users WHERE username = $1", username)
+	usernamePath := c.Param("username")
+	rows, err := db.Query("SELECT id, username, name, birth_date, registration_date FROM users WHERE username = $1", usernamePath)
 
 	defer rows.Close()
+
+	var id int
+	var username, name, birth_date, registration_date string
+	var user_unmarshal User
+
 	for rows.Next() {
-		var id int
-		var name, birth_date, registration_date string
-
-		err = rows.Scan(&id, &name, &birth_date, &registration_date)
-
-		user_unmarshal := User{
-			Id:                id,
-			Name:              name,
-			Birth_date:        birth_date,
-			Registration_date: registration_date,
-		}
-
-		fmt.Println(user_unmarshal)
-
-		b, err := json.Marshal(user_unmarshal)
-		if err != nil {
-			fmt.Println("error:", err)
-		}
-
-		os.Stdout.Write(b)
+		err = rows.Scan(&id, &username, &name, &birth_date, &registration_date)
+		user_unmarshal.Id = id
+		user_unmarshal.Username = username
+		user_unmarshal.Name = name
+		user_unmarshal.Birth_date = birth_date
+		user_unmarshal.Registration_date = registration_date
 	}
+
+	if user_unmarshal.Username == usernamePath {
+		c.IndentedJSON(http.StatusOK, user_unmarshal)
+		return
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "User not found"})
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/whoiam/:username", getUserByUsername)
+
+	router.Run("localhost:3000")
 }
